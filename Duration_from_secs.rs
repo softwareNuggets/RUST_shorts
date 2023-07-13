ea915d7366cbf5c8e273a6f10c23154f9dc04d1a
SoftwareNuggets <92217545+softwareNuggets@users.noreply.github.com>
2023-07-13 12:33:49 -0400

    Duration_from_secs.rs
    How to pause your application for n seconds using Duration::from_secs and thread::sleep.

---
+++ Duration_from_secs.rs
@@ -0,0 +1,20 @@
+# GitHub Account
+# https://github.com/softwareNuggets/Rust_shorts/
+# Duration_from_secs.rs
+
+use std::time::{Duration};
+use std::thread;
+
+fn main() {
+
+	let duration = Duration::from_secs(3);
+	thread::sleep(duration);
+
+	dbg!(&duration);
+	dbg!(&duration.as_micros());
+	dbg!(&duration.as_millis());
+	dbg!(&duration.as_nanos());
+	dbg!(&duration.as_secs());
+	dbg!(&duration.as_secs_f32());
+	dbg!(&duration.as_secs_f64());
+}
